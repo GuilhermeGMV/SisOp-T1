54504d8a43bf6072dbf16de420125321cfb59462
GuilhermeGMV <gui141202@gmail.com>
2025-09-05 18:27:43 -0300

    feat: split sistema classes

---
+++ Sistema.java
@@ -0,0 +1,52 @@
+// PUCRS - Escola Politécnica - Sistemas Operacionais
+// Prof. Fernando Dotti
+// Código fornecido como parte da solução do projeto de Sistemas Operacionais
+//
+// Estrutura deste código:
+//    As classes foram separadas em arquivos diferentes para melhor organização.
+//    O Sistema agora instancia e coordena os componentes:
+//           HW (Hardware) - constituído de CPU e Memory
+//           SO (Sistema Operacional) - com InterruptHandling, SysCallHandling e Utilities  
+//           Programs - programas disponíveis para execução
+//
+// Veja o main. Ele instancia o Sistema com os elementos mencionados acima.
+// em seguida solicita a execução de algum programa com loadAndExec
+
+public class Sistema {
+    // ------------------- S I S T E M A
+    // --------------------------------------------------------------------
+
+    public HW hw;
+    public SO so;
+    public Programs progs;
+
+    public Sistema(int tamMem) {
+        hw = new HW(tamMem);           // memoria do HW tem tamMem palavras
+        so = new SO(hw);
+        hw.cpu.setUtilities(so.utils); // permite cpu fazer dump de memoria ao avancar
+        progs = new Programs();
+    }
+
+    public void run() {
+        so.utils.loadAndExec(progs.retrieveProgram("fatorialV2"));
+
+        // so.utils.loadAndExec(progs.retrieveProgram("fatorial"));
+        // fibonacci10,
+        // fibonacci10v2,
+        // progMinimo,
+        // fatorialWRITE, // saida
+        // fibonacciREAD, // entrada
+        // PB
+        // PC, // bubble sort
+    }
+    // ------------------- S I S T E M A - fim
+    // --------------------------------------------------------------
+    // -------------------------------------------------------------------------------------------------------
+
+    // -------------------------------------------------------------------------------------------------------
+    // ------------------- instancia e testa sistema
+    public static void main(String args[]) {
+        Sistema s = new Sistema(1024);
+        s.run();
+    }
+}
---
+++ SysCallHandling.java
@@ -0,0 +1,31 @@
+public class SysCallHandling {
+    private final HW hw; // referencia ao hw se tiver que setar algo
+
+    public SysCallHandling(HW _hw) {
+        hw = _hw;
+    }
+
+    public void stop() { // chamada de sistema indicando final de programa
+                         // nesta versao cpu simplesmente pára
+        System.out.println("                                               SYSCALL STOP");
+    }
+
+    public void handle() { // chamada de sistema 
+                           // suporta somente IO, com parametros 
+                           // reg[8] = in ou out    e reg[9] endereco do inteiro
+        System.out.println("SYSCALL pars:  " + hw.cpu.reg[8] + " / " + hw.cpu.reg[9]);
+
+        switch (hw.cpu.reg[8]) {
+            case 1:
+                // leitura ...
+                break;
+            case 2:
+                // escrita - escreve o conteuodo da memoria na posicao dada em reg[9]
+                System.out.println("OUT:   "+ hw.mem.pos[hw.cpu.reg[9]].p);
+                break;
+            default:
+                System.out.println("  PARAMETRO INVALIDO");
+                break;
+        }
+    }
+}
